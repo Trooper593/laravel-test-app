f35b026f26ddef63aa94de8543e8a5e9da1a55b7
Dmitry Kuznetsov <dimushka5.93@mail.ru>
2022-09-12 21:40:18 +0300

    lesson2: bootstrap

--- resources/views/inc/hero.blade.php
+++ resources/views/inc/hero.blade.php
@@ -1,4 +1,3 @@
-@section('hero')
-<div class="hero">
-    Hero block
+<div class="jumbotrone">
+    <div class="container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad alias assumenda at eum, eveniet ex expedita mollitia, natus nesciunt nihil odio praesentium quae, quas quidem quod totam vitae voluptate.</div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,33 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <title>@yield('title-block')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css" class="href">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>@yield('title-block')</title>
 </head>
 <body>
-@yield('content')
+@include('inc.header')
 
 @if(Request::is('/'))
     @include('inc.hero')
 @endif
 
-@include('inc.aside')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-8">
+            @yield('content')
+        </div>
+        <div class="col-4">
+            @include('inc.aside')
+        </div>
+    </div>
+</div>
+
+@include('inc.footer')
 </body>
 </html>
