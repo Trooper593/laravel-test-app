9db1f164b5d036b36913dfb76b2b0b0c1846480d
Dmitry Kuznetsov <dimushka5.93@mail.ru>
2022-09-16 15:55:38 +0300

    laravel\breeze installation
    composer require laravel/breeze --dev
    php artisan breeze:install react
    php artisan migrate
    npm install
    npm run dev

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,16 @@ use App\Http\Controllers;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
